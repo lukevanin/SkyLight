eece52cca244b17876c83e9276ec4d0f83ce2ce7
Luke Van In <lukevanin@gmail.com>
2023-01-02 17:07:44 +0200

    Match SIFT descriptors

--- SkySightTests/Tests/DescriptorTests.swift
+++ SkySightTests/Tests/DescriptorTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import simd
 
 @testable import SkySight
 
@@ -41,6 +42,8 @@ final class DescriptorTests: SharedTestCase {
 
         let referenceDescriptors = try loadDescriptors(filename: "butterfly-descriptors")
         
+        matchDescriptors(detected: foundDescriptors, reference: referenceDescriptors)
+        
         attachImage(
             name: "descriptors",
             uiImage: drawDescriptors(
@@ -49,28 +52,140 @@ final class DescriptorTests: SharedTestCase {
                 foundDescriptors: foundDescriptors
             )
         )
-
-//        for (scale, octave) in subject.octaves.enumerated() {
-//
-//            for (index, texture) in octave.keypointTextures.enumerated() {
-//
-//                attachImage(
-//                    name: "keypoints(\(scale), \(index))",
-//                    uiImage: ciContext.makeUIImage(
-//                        ciImage: CIImage(
-//                            mtlTexture: texture,
-//                            options: [
-//                                .colorSpace: CGColorSpace(name: CGColorSpace.genericGrayGamma2_2)!
-//                            ]
-//                        )!
-//                            .oriented(.downMirrored)
-//                            .smearColor()
-//                    )
-//                )
-//            }
-//
-//        }
+    }
+    
+    
+    func testMatches() throws {
         
+        let inputTexture = try device.loadTexture(name: "butterfly", extension: "png", srgb: false)
+        let configuration = SIFT.Configuration(
+            inputSize: IntegralSize(
+                width: inputTexture.width,
+                height: inputTexture.height
+            )
+        )
+        let subject = SIFT(device: device, configuration: configuration)
+        let keypoints = subject.getKeypoints(inputTexture)
+        let foundDescriptors = subject.getDescriptors(keypoints: keypoints)
+        print("Found", foundDescriptors.count, "descriptors")
+
+        let referenceImage: CGImage = {
+            let originalImage = CIImage(
+                mtlTexture: inputTexture,
+                options: [
+                    CIImageOption.colorSpace: CGColorSpace(name: CGColorSpace.sRGB)!,
+                ]
+            )!
+                .oriented(.downMirrored)
+                .smearColor()
+            let cgImage = ciContext.makeCGImage(ciImage: originalImage)
+            return cgImage
+        }()
+
+        let referenceDescriptors = try loadDescriptors(filename: "butterfly-descriptors")
+        print("Loaded \(referenceDescriptors.count) reference descriptors")
+        
+        // matchDescriptors(detected: foundDescriptors, reference: referenceDescriptors)
+        
+        print("Finding matches")
+        let matches = SIFTDescriptor.match(
+            source: filter(foundDescriptors),
+            target: referenceDescriptors,
+            absoluteThreshold: 300,
+            relativeThreshold: 0.6
+        )
+        print("Found \(matches.count) matches")
+
+        print("drawing matches")
+        attachImage(
+            name: "matches",
+            uiImage: drawMatches(
+                sourceImage: referenceImage,
+                targetImage: referenceImage,
+                matches: filter(matches, every: 5)
+            )
+        )
+    }
+    
+    private func filter<E>(_ input: Array<E>, every step: Int = 10, limit: Int = 100) -> [E] {
+        Array(input.enumerated().filter({ $0.offset % step == 0 }).map({ $0.element }).prefix(limit))
+    }
+    
+    private func matchDescriptors(detected: [SIFTDescriptor], reference: [SIFTDescriptor]) {
+        var pass = 0
+        var fail = 0
+        var total = 0
+        
+        for i in 0 ..< detected.count {
+            let d = detected[i]
+            
+            var bestMatchDistance = Float.greatestFiniteMagnitude
+            var secondBestMatchDistance = Float.greatestFiniteMagnitude
+            var bestMatch: SIFTDescriptor!
+            var secondBestMatch: SIFTDescriptor!
+
+            for r in reference {
+                
+                var magnitude = 0
+                
+                for k in 0 ..< 128 {
+                    
+                    let fd = d.features[k]
+                    let fr = r.features[k]
+                    let d = fd - fr
+                    magnitude += (d * d)
+                }
+                
+                let distance = sqrt(Float(magnitude))
+                
+                guard distance < 300 else {
+                    // Points do not match
+                    continue
+                }
+                
+                if distance < bestMatchDistance {
+                    secondBestMatchDistance = bestMatchDistance
+                    bestMatchDistance = distance
+                    secondBestMatch = bestMatch
+                    bestMatch = r
+                }
+            }
+            
+            guard let bestMatch = bestMatch else {
+                continue
+            }
+            guard let secondBestMatch = secondBestMatch else {
+                continue
+            }
+            
+            let r = bestMatchDistance / secondBestMatchDistance
+            
+            guard r < 0.6 else {
+                continue
+            }
+
+            let m: SIFTDescriptor! = secondBestMatch
+            let a = d.keypoint.absoluteCoordinate
+            let b = m.keypoint.absoluteCoordinate
+            let c = simd_abs(b - a)
+            let distance = simd_length(b - a)
+
+            let icon: String
+            if distance < 10.0 {
+                pass += 1
+                icon = "✅"
+            }
+            else {
+                fail += 1
+                icon = "❌"
+            }
+            total += 1
+            
+            print("\(icon) #\(i) @\(a) == \(b) Δ\(bestMatchDistance ) \(c) \(distance)")
+        }
+        
+        print("pass: \(pass) \(Float(pass) / Float(total))")
+        print("fail: \(fail) \(Float(fail) / Float(total))")
     }
     
     
--- SkySightTests/Tests/SharedTestCase.swift
+++ SkySightTests/Tests/SharedTestCase.swift
@@ -234,7 +234,111 @@ func drawDescriptors(
 }
 
 
-private func drawDescriptors(cgContext: CGContext, color: UIColor, descriptors: [SIFTDescriptor]) {
+func drawMatches(
+    sourceImage: CGImage,
+    targetImage: CGImage,
+    overlayColor: UIColor = UIColor.black.withAlphaComponent(0.8),
+    sourceColor: UIColor = UIColor.green,
+    targetColor: UIColor = UIColor.red,
+    matches: [SIFTCorrespondence]
+) -> UIImage {
+    
+    precondition(targetImage.width == sourceImage.width)
+    precondition(targetImage.height == sourceImage.height)
+    let imageSize = CGSize(width: sourceImage.width, height: sourceImage.height)
+    let bounds = CGRect(x: 0, y: 0, width: imageSize.width * 2, height: imageSize.height)
+    
+    let sourceOffset = CGPoint(x: 0, y: 0)
+    let targetOffset = CGPoint(x: imageSize.width, y: 0)
+    let sourceBounds = CGRect(origin: sourceOffset, size: imageSize)
+    let targetBounds = CGRect(origin: targetOffset, size: imageSize)
+
+    let sourceDescriptors = matches.map { $0.source }
+    let targetDescriptors = matches.map { $0.target }
+    
+    let colors: [UIColor] = [
+        .systemRed, .systemOrange, .systemYellow, .systemGreen, .systemTeal, .systemBlue, .systemPurple, .systemIndigo
+    ]
+    let radius = CGFloat(2)
+
+    let renderer = UIGraphicsImageRenderer(size: bounds.size)
+    let uiImage = renderer.image { context in
+        let cgContext = context.cgContext
+        
+        // Draw images
+        cgContext.saveGState()
+        cgContext.scaleBy(x: 1, y: -1)
+        cgContext.translateBy(x: 0, y: -bounds.height)
+        cgContext.draw(sourceImage, in: sourceBounds)
+        cgContext.draw(targetImage, in: targetBounds)
+        cgContext.restoreGState()
+
+        // Overlay
+        cgContext.saveGState()
+        cgContext.setBlendMode(.multiply)
+        cgContext.setFillColor(overlayColor.cgColor)
+        cgContext.fill([bounds])
+        cgContext.restoreGState()
+
+        // Descriptors
+        drawDescriptors(cgContext: cgContext, at: sourceOffset, color: sourceColor, descriptors: sourceDescriptors)
+        drawDescriptors(cgContext: cgContext, at: targetOffset, color: targetColor, descriptors: targetDescriptors)
+        
+        cgContext.saveGState()
+        cgContext.setLineWidth(1)
+        cgContext.setBlendMode(.screen)
+
+        for i in 0 ..< matches.count {
+            let n = i % (colors.count - 1)
+            let color = colors[n]
+            
+            let match = matches[i]
+            let ks = match.source.keypoint.absoluteCoordinate
+            let kt = match.target.keypoint.absoluteCoordinate
+            let ps = CGPoint(
+                x: sourceOffset.x + CGFloat(ks.x),
+                y: sourceOffset.y + CGFloat(ks.y)
+            )
+            let pt = CGPoint(
+                x: targetOffset.x + CGFloat(kt.x),
+                y: targetOffset.y + CGFloat(kt.y)
+            )
+            let bs = CGRect(
+                x: ps.x - radius,
+                y: ps.y - radius,
+                width: radius * 2,
+                height: radius * 2
+            )
+            let bt = CGRect(
+                x: pt.x - radius,
+                y: pt.y - radius,
+                width: radius * 2,
+                height: radius * 2
+            )
+
+            cgContext.move(to: ps)
+            cgContext.addLine(to: pt)
+
+            cgContext.addEllipse(in: bs)
+            cgContext.addEllipse(in: bt)
+            
+            cgContext.setStrokeColor(color.cgColor)
+            cgContext.strokePath()
+        }
+        
+        cgContext.restoreGState()
+
+    }
+    return uiImage
+}
+
+
+private func drawDescriptors(
+    cgContext: CGContext,
+    at offset: CGPoint = .zero,
+    color: UIColor,
+    descriptors: [SIFTDescriptor]
+) {
     cgContext.saveGState()
     cgContext.setLineWidth(1)
     cgContext.setStrokeColor(color.cgColor)
@@ -243,8 +347,8 @@ private func drawDescriptors(cgContext: CGContext, color: UIColor, descriptors: 
         let keypoint = descriptor.keypoint
         let radius = 1.5 * CGFloat(keypoint.sigma)
         let center = CGPoint(
-            x: CGFloat(keypoint.absoluteCoordinate.x),
-            y: CGFloat(keypoint.absoluteCoordinate.y)
+            x: offset.x + CGFloat(keypoint.absoluteCoordinate.x),
+            y: offset.y + CGFloat(keypoint.absoluteCoordinate.y)
         )
         let bounds = CGRect(
             x: center.x - radius,
